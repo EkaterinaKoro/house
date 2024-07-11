4e2b628acda6c57431ac05eec531022bdb8abb9a
Kate <cimonyu@mail.ru>
2024-07-11 17:50:32 +0300

    редактор

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,24 @@
 public class Main {
     public static void main(String[] args) {
 
-                // Начальная сумма на счету клиента
-                double initialBalance = 200.0;
-                // Сумма пополнения
-                double topUpAmount = 1200.0;
+        // Начальная сумма на счету клиента
+        double initialBalance = 200.0;
+        // Сумма пополнения
+        double topUpAmount = 1_200.0;
 
-                // Вычисление бонусов
-                double bonus = (topUpAmount > 1000) ? (int)(topUpAmount / 100) : 0;
+        // Вычисление бонусов
+        double bonus = (topUpAmount > 1_000) ? (int) (topUpAmount / 100) : 0;
 
-                // Итоговая сумма на счету клиента
-                double finalBalance = initialBalance + topUpAmount + bonus;
+        // Итоговая сумма на счету клиента
+        double finalBalance = initialBalance + topUpAmount + bonus;
 
-                // Вывод результатов
-                System.out.println("Начальная сумма на счету: " + initialBalance + " рублей");
-                System.out.println("Сумма пополнения: " + topUpAmount + " рублей");
-                System.out.println("Количество бонусных рублей: " + bonus);
-                System.out.println("Итоговая сумма на счету: " + finalBalance + " рублей");
-            }
-        }
+        // Вывод результатов
+        System.out.println("Начальная сумма на счету: " + initialBalance + " рублей");
+        System.out.println("Сумма пополнения: " + topUpAmount + " рублей");
+        System.out.println("Количество бонусных рублей: " + bonus);
+        System.out.println("Итоговая сумма на счету: " + finalBalance + " рублей");
+    }
+}
 
 
 
