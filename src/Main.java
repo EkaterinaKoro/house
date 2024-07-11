cd4a2cff61a3066e268383eddaee8837a4ef3349
Kate <cimonyu@mail.ru>
2024-07-11 17:10:29 +0300

    house.md

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                // Начальная сумма на счету клиента
+                double initialBalance = 200.0;
+                // Сумма пополнения
+                double topUpAmount = 1200.0;
+
+                // Вычисление бонусов
+                double bonus = (topUpAmount > 1000) ? (int)(topUpAmount / 100) : 0;
+
+                // Итоговая сумма на счету клиента
+                double finalBalance = initialBalance + topUpAmount + bonus;
+
+                // Вывод результатов
+                System.out.println("Начальная сумма на счету: " + initialBalance + " рублей");
+                System.out.println("Сумма пополнения: " + topUpAmount + " рублей");
+                System.out.println("Количество бонусных рублей: " + bonus);
+                System.out.println("Итоговая сумма на счету: " + finalBalance + " рублей");
+            }
         }
-    }
-}
+
+
+
+
+
+
+
+
